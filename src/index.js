95333cda87bff88cdcfd09676d1e64757a9a0086
Aaron Brinkley <AaronBrinkley@macbook-air.mynetworksettings.com>
2023-02-02 14:13:02 -0500

    4th Serve

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import CatchAllRoute from './App.js';
 import reportWebVitals from './reportWebVitals';
+import BrowserRouter from 'react-router-dom/BrowserRouter'
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <CatchAllRoute />
-  </React.StrictMode>,
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
